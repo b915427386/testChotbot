6fc886a99a90641959d150f45b343dd64049e39c
b915427386 <b915427386@gmail.com>
2018-09-19 19:52:39 +0800

    1.01

--- start_service.sh
+++ start_service.sh
@@ -2,19 +2,19 @@
 #!/bin/bash
 cd /home/ec2-user/deploy
 echo "Running service container "
-docker-compose -f $vision/docker-compose.yml up -d
+docker-compose -f $version/docker-compose.yml up -d
 echo "Testing service container "
 docker ps > /home/es2-user/container_log
 containerNum=cat /home/es2-user/container_log|wc -l
-for ((i=0;i<3;i++))
-do
-  sleep 5
-  if [$containerNum==6];then
-    echo "remove image second "
-    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:$pvision
-    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:$pvision
-    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:$pvision
-    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:$pvision
-    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:$pvision
-  fi
-done
+#for ((i=0;i<3;i++))
+#do
+#  sleep 5
+#  if [$containerNum==6];then
+#    echo "remove image second "
+#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:$pvision
+#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:$pvision
+#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:$pvision
+#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:$pvision
+#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:$pvision
+#  fi
+#done
--- stop_service.sh
+++ stop_service.sh
@@ -3,15 +3,15 @@
 echo "stop service container "
 pversion=`cat VERSION|cut -d ';' -f 2`
 version=`cat VERSION|cut -d ';' -f 1`
-docker_compose_file="$pvision/docker-compose.yml"
+docker_compose_file="$pversion/docker-compose.yml"
 echo "remove container "
 docker-compose -f $docker_compose_file rm -sf
 echo "re-tag image "
-docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:$pvision
-docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:$pvision
-docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:$pvision
-docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:$pvision
-docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:$pvision
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:$pversion
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:$pversion
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:$pversion
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:$pversion
+docker tag 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:$pversion
 echo "remove image first "
 docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
 docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:latest

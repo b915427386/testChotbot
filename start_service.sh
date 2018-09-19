77e48d5db86afbae62a856d79ece44ecbea5daaa
b915427386 <b915427386@gmail.com>
2018-09-19 22:18:00 +0800

    1.04

--- start_service.sh
+++ start_service.sh
@@ -1,20 +1,25 @@
 
 #!/bin/bash
 cd /home/ec2-user/deploy
+version=`cat VERSION|cut -d ';' -f 1`
 echo "Running service container "
 docker-compose -f $version/docker-compose.yml up -d
 echo "Testing service container "
-docker ps > /home/es2-user/container_log
-containerNum=cat /home/es2-user/container_log|wc -l
-#for ((i=0;i<3;i++))
-#do
-#  sleep 5
-#  if [$containerNum==6];then
-#    echo "remove image second "
-#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:$pvision
-#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:$pvision
-#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:$pvision
-#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:$pvision
-#    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:$pvision
-#  fi
-#done
+touch /home/ec2-user/containerLog
+sudo docker ps > /home/ec2-user/containerLog
+containerNum=`cat /home/ec2-user/containerLog|wc -l`
+for ((i=0;i<3;i++))
+do
+  sleep 5
+  if [ $containerNum == 6 ];then
+    echo "remove image second "
+    pversion=`cat VERSION|cut -d ';' -f 2`
+    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:$pversion
+    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:$pversion
+    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:$pversion
+    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:$pversion
+    docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:$pversion
+    break
+  fi
+done
+

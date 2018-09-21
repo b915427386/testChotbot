b48bf2dbc1ab3d7e8a9780b4a9626c72ec50f727
b915427386 <b915427386@gmail.com>
2018-09-21 15:24:26 +0800

    20180921152426

--- build_service.sh
+++ build_service.sh
@@ -5,5 +5,5 @@ $(aws ecr get-login --no-include-email --region ap-northeast-1)
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/mysql:latest
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/redis:latest
-docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:0.0.2
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest
